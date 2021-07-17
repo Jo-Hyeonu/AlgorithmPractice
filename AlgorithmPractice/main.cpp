187ba5bad99e986823aa6969f13e02c3db38c17d
Jo-Hyeonu <rewolfnus0824@naver,com>
2021-07-18 02:12:14 +0900

    1012

--- AlgorithmPractice/main.cpp
+++ AlgorithmPractice/main.cpp
@@ -6,9 +6,57 @@
 //
 
 #include <iostream>
+#include<cstring>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+bool check[51][51];
+int dx[] = {-1,1,0,0};
+int dy[] = {0,0,-1,1};
+int field[51][51];
+int M,N,K;
+
+void dfs(int x, int y){
+    check[x][y] = true;
+    for(int i = 0; i < 4; i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(nx >= 0 && nx < N && ny >= 0 && ny < M) {
+            if(field[nx][ny] == 1) {
+                if(!check[nx][ny]){
+                    dfs(nx, ny);
+                }
+            }
+        }
+        else{
+            continue;
+        }
+    }
+}
+
+
+int main(){
+    int NOTC = 0;
+    cin >> NOTC;
+    while(NOTC--) {
+        cin >> M >> N >> K;
+        int result = 0;
+        for(int i = 0; i < K; i++){
+            int a, b;
+            cin >> a >> b;
+            field[b][a] = 1;
+        }
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                if(field[i][j] == 1){
+                    if(!check[i][j]){
+                        result++;
+                        dfs(i,j);
+                    }
+                }
+            }
+        }
+        cout << result << endl;
+        memset(check, false, sizeof(check));
+        memset(field, 0, sizeof(field));
+    }
 }
