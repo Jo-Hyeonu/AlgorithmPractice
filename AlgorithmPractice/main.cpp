4fb60e4b188c3f6597041b58c9b64488c9c94dbe
Jo-Hyeonu <rewolfnus0824@naver,com>
2021-07-19 03:51:07 +0900

    2798

--- AlgorithmPractice/main.cpp
+++ AlgorithmPractice/main.cpp
@@ -6,57 +6,28 @@
 //
 
 #include <iostream>
-#include<cstring>
+
 using namespace std;
 
-bool check[51][51];
-int dx[] = {-1,1,0,0};
-int dy[] = {0,0,-1,1};
-int field[51][51];
-int M,N,K;
+int main() {
+    int N, M;
+    int a[100];
+    cin >> N >> M;
+    for(int i = 0 ; i < N ; i++) {
+        cin >> a[i];
+    }
 
-void dfs(int x, int y){
-    check[x][y] = true;
-    for(int i = 0; i < 4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(nx >= 0 && nx < N && ny >= 0 && ny < M) {
-            if(field[nx][ny] == 1) {
-                if(!check[nx][ny]){
-                    dfs(nx, ny);
+    int result = 0;
+
+    for(int i = 0 ; i < N-2 ; i++){
+        for(int j = i+1 ; j < N-1; j++){
+            for(int k = j+1 ; k < N; k++) {
+                int temp = a[i] + a[j] + a[k];
+                if( temp <= M && temp > result){
+                    result = temp;
                 }
             }
         }
-        else{
-            continue;
-        }
     }
-}
-
-
-int main(){
-    int NOTC = 0;
-    cin >> NOTC;
-    while(NOTC--) {
-        cin >> M >> N >> K;
-        int result = 0;
-        for(int i = 0; i < K; i++){
-            int a, b;
-            cin >> a >> b;
-            field[b][a] = 1;
-        }
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                if(field[i][j] == 1){
-                    if(!check[i][j]){
-                        result++;
-                        dfs(i,j);
-                    }
-                }
-            }
-        }
-        cout << result << endl;
-        memset(check, false, sizeof(check));
-        memset(field, 0, sizeof(field));
-    }
+    cout << result << endl;
 }
