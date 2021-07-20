519e76c989de1672273512633d411c5f2afa0f83
Jo-Hyeonu <rewolfnus0824@naver,com>
2021-07-20 23:09:32 +0900

    7568

--- AlgorithmPractice/main.cpp
+++ AlgorithmPractice/main.cpp
@@ -11,39 +11,10 @@ using namespace std;
 
 int main() {
     int N;
-    int result = 0;
-    cin >> N;
+    int a[50][2];
     
-    if( N < 100 ) {
-        result = N;
-    }
-    else {
-        result = 99;
-        for(int i = 1; i < 10 ; i++){
-            int temp = N;
-            for(int j = i; (2*j-i) < 10; j++) {
-                temp = i*100 + j*10 + (2*j-i);
-                if( N >= temp){
-                    result++;
-                }
-                else {
-                    break;
-                }
-            }
-            if( N < temp){
-                break;
-            }
-        }
+    while(N--) {
         
-        for(int i = 9; i > 0 ; i--) {
-            for(int j = i-1 ; (2*j-i) >= 0 ; j--) {
-                int temp = i*100 + j*10 + (2*j-i);
-                if(N >= temp) {
-                    result++;
-                }
-            }
-        }
     }
-    cout << result << endl;
-    return 0;
+    
 }
