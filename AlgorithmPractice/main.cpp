b3bbda1928df69c8b22348048e51c725f219bb91
Jo-Hyeonu <rewolfnus0824@naver,com>
2021-07-22 01:58:18 +0900

    1018

--- AlgorithmPractice/main.cpp
+++ AlgorithmPractice/main.cpp
@@ -6,26 +6,42 @@
 //
 
 #include <iostream>
-#include <cstring>
+
 using namespace std;
 
 int N, M;
-char a[51][51]; // N, M
-int result = 0;
-int dx[] = {-1,1,0,0};
-int dy[] = {0,0,-1,1};
+int a[51][51]; // N, M
+int result = 2500;
 
-
-int dfs(int n, int m) {
-    int cnt = 0;
-    if (a[n][m] == "W"){
-        
+int minV(int a, int b) {
+    if (a <= b) {
+        return a;
     }
     else {
-        
+        return b;
     }
-    
-    
+}
+
+int startW[8][8] = {{0,1,0,1,0,1,0,1},{1,0,1,0,1,0,1,0},{0,1,0,1,0,1,0,1},{1,0,1,0,1,0,1,0},{0,1,0,1,0,1,0,1},{1,0,1,0,1,0,1,0},{0,1,0,1,0,1,0,1},{1,0,1,0,1,0,1,0}};
+int startB[8][8] = {{1,0,1,0,1,0,1,0},{0,1,0,1,0,1,0,1},{1,0,1,0,1,0,1,0},{0,1,0,1,0,1,0,1},{1,0,1,0,1,0,1,0},{0,1,0,1,0,1,0,1},{1,0,1,0,1,0,1,0},{0,1,0,1,0,1,0,1}};
+
+
+
+
+int check(int n, int m) {
+    int cntW = 0;
+    int cntB = 0;
+    for(int i = 0; i < 8 ; i++) {
+        for(int j = 0; j < 8 ; j++){
+            if(startW[i][j] != a[n+i][m+j]){
+                cntW++;
+            }
+            if(startB[i][j] != a[n+i][m+j]){
+                cntB++;
+            }
+        }
+    }
+    return min(cntW,cntB);
 }
 
 
@@ -35,16 +51,24 @@ int main() {
     cin >> N >> M;
     for(int i = 0; i < N ; i++) {
         for(int j = 0 ; j < M ; j++){
-            cin >> a[i][j];
+            char temp;
+            cin >> temp;
+            if(temp == 'W'){
+                a[i][j] = 0;
+            }
+            else {
+                a[i][j] = 1;
+            }
         }
     }
     for(int i = 0; i < N - 7 ; i++) {
         for(int j = 0 ; j < M - 7 ; j++){
-            if(dfs(i,j) < result){
-                result = dfs(i,j);
+            int temp = check(i,j);
+            if(temp < result){
+                result = temp;
             }
         }
     }
-    cout << dfs << endl;
+    cout << result << endl;
     
 }
