0e3afca4e0d143f0e2b50c58c3b7ed9c0dc5296e
Jo-Hyeonu <rewolfnus0824@naver,com>
2021-07-21 23:43:56 +0900

    1018

--- AlgorithmPractice/main.cpp
+++ AlgorithmPractice/main.cpp
@@ -6,15 +6,45 @@
 //
 
 #include <iostream>
-
+#include <cstring>
 using namespace std;
 
-int main() {
-    int N;
-    int a[50][2];
-    
-    while(N--) {
+int N, M;
+char a[51][51]; // N, M
+int result = 0;
+int dx[] = {-1,1,0,0};
+int dy[] = {0,0,-1,1};
+
+
+int dfs(int n, int m) {
+    int cnt = 0;
+    if (a[n][m] == "W"){
         
     }
+    else {
+        
+    }
+    
+    
+}
+
+
+
+int main() {
+    
+    cin >> N >> M;
+    for(int i = 0; i < N ; i++) {
+        for(int j = 0 ; j < M ; j++){
+            cin >> a[i][j];
+        }
+    }
+    for(int i = 0; i < N - 7 ; i++) {
+        for(int j = 0 ; j < M - 7 ; j++){
+            if(dfs(i,j) < result){
+                result = dfs(i,j);
+            }
+        }
+    }
+    cout << dfs << endl;
     
 }
