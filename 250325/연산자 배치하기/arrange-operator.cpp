1a939130bc4408f97e1b8ef1c1991c1a4cfce687
Jo-Hyeonu <84915598+Jo-Hyeonu@users.noreply.github.com>
2025-03-25 01:06:51 +0900

    Title: 연산자 배치하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250325/연산자 배치하기/arrange-operator.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+vector<int> num;
+int op[3]; // + - *
+
+int maxret = -1000000001;
+int minret = 1000000001;
+
+void dfs(int cnt, int op[3], int cur){
+    if(cnt == n-1){
+        if(cur > maxret){
+            maxret = cur;
+            return;
+        }
+
+        if(cur < minret){
+            minret = cur;
+            return;
+        }
+
+    }
+
+    for(int i = 0 ; i< 3; ++i){
+        if(op[i] > 0){
+            op[i]--;
+            if(i == 0){
+                dfs(cnt+1, op, cur+num[cnt+1]);
+            }
+            else if(i == 1){
+                dfs(cnt+1, op, cur-num[cnt+1]);
+            }
+            else{
+                dfs(cnt+1, op, cur*num[cnt+1]);
+            }
+            op[i]++;
+        }
+    }
+    
+}
+
+
+int main() {
+    cin >> n;
+    num.resize(n);
+    for(int i =0  ;i < n ;++i){
+        cin >> num[i];
+    }
+
+    for(int i = 0 ; i < 3; ++i){
+        cin >> op[i];
+    }
+
+    
+
+    dfs(0,op, num[0]);
+
+
+    cout << minret << " " << maxret;
+
+    return 0;
+}
