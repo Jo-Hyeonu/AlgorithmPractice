48403eceaff74a84472fe296379f3c5da1e23385
Jo-Hyeonu <84915598+Jo-Hyeonu@users.noreply.github.com>
2025-03-26 19:31:20 +0900

    Title: 돌아가는 팔각 의자, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250326/돌아가는 팔각 의자/rounding-eight-angle.cpp
+++ 250326/돌아가는 팔각 의자/rounding-eight-angle.cpp
@@ -7,6 +7,15 @@ vector<int> v1,v2,v3,v4;
 
 int main() {
     v1.resize(8);
-    v1.resize(8);
+    v2.resize(8);
+    v3.resize(8);
+    v4.resize(8);
+
+    for(int i = 0 ; i < 8; ++i){
+        cin >> v1.[i];
+    }
+        for(int i = 0 ; i < 8; ++i){
+        cin >> v1.[i];
+    }
     return 0;
 }
