528618068ee6afc5b77dc84870b36d0d3824c287
Jo-Hyeonu <84915598+Jo-Hyeonu@users.noreply.github.com>
2025-04-04 23:48:28 +0900

    Title: 토스트 계란틀, Time: 1016ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 250404/토스트 계란틀/toast-eggmold.cpp
+++ 250404/토스트 계란틀/toast-eggmold.cpp
@@ -12,74 +12,103 @@ int dx[4] = { 1, 0, -1, 0};
 int dy[4] = { 0, -1, 0, 1};
 
 int board[51][51];
+int same[2501];
+int ret = 0;
+int cnt  = 0;
 
-bool bfs() {
-    vector<vector<int>> vst(n, vector<int>(n, 0));
-    int cnt = 0;
-    bool isMoved = false;
-
-    for(int i = 0 ; i < n ; ++i){
-        for(int j = 0 ; j < n ; ++j){
-            if(vst[i][j] == 0){
-                cnt++;
-                queue<pair<int, int>> q;
-                vector<pair<int, int>> group;
-
-                q.push({i,j});
-                vst[i][j] = cnt;
-                group.push_back({i, j});
-
-                int nume = board[i][j], deno = 1;
-
-                while(!q.empty()){
-                    auto [x, y] = q.front();
-                    q.pop();
-
-                    for(int k = 0 ; k < 4; ++k){
-                        int nx = x + dx[k];
-                        int ny = y + dy[k];
-
-                        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                        if(vst[nx][ny] != 0) continue;
-
-                        int diff = abs(board[nx][ny] - board[x][y]);
-                        if(diff < l || diff > r) continue;
-
-                        vst[nx][ny] = cnt;
-                        q.push({nx, ny});
-                        group.push_back({nx, ny});
-                        nume += board[nx][ny];
-                        deno++;
-                        isMoved = true;
-                    }
-                }
-
-                int newVal = nume / deno;
-                for(auto [x, y] : group){
-                    board[x][y] = newVal;
-                }
-            }
-        }
-    }
-
-    return isMoved;
-}
+int bfs(void);
 
 int main() {
     cin >> n >> l >> r;
 
-    for(int i = 0 ; i < n; i++){
+    for(int i =0  ;i < n; i++){
         for(int j = 0; j < n; j++){
             cin >> board[i][j];
         }
     }
 
-    int ret = 0;
-    while (true) {
-        if (!bfs()) break;
-        ret++;  // 하루 증가
+    while(1){
+        int check = bfs();
+
+        if(check == 0){
+            break;
+        }
+        ret++;
+    }
+    
+    cout << ret;
+
+    return 0;
+}
+
+int bfs(void){
+    int ch = 0;
+    vector<vector<int>> vst;
+    vst.resize(n);
+    for(int i = 0 ; i< n ;++i){
+        vst[i].resize(n);
     }
 
-    cout << ret << endl;
-    return 0;
+    for(int i = 0 ; i < n ;++i){
+        for(int j = 0 ; j < n; ++j){
+            vst[i][j] = 0;
+        }
+    }
+
+    cnt = 0;
+
+    for(int i = 0 ; i < n ; ++i){
+        for(int j = 0 ; j < n ;++j){
+
+            if(vst[i][j] == 0){
+                cnt++;
+                queue<pair<int, int>> q;
+                q.push(make_pair(i,j));
+                vst[i][j] = cnt;
+
+                int nume = board[i][j], deno = 1;
+
+                while(!q.empty()){
+                    pair<int,int> temp = q.front();
+                    q.pop();
+
+                    for(int k = 0 ; k < 4; ++k){
+                        int nx , ny;
+                        nx = temp.first + dx[k];
+                        ny = temp.second + dy[k];
+
+                        if(nx < 0 || nx >= n || ny < 0 || ny >= n){
+                            continue;
+                        }
+
+                        if(vst[nx][ny] != 0){
+                            continue;
+                        }
+
+                        int diff = abs(board[nx][ny] - board[temp.first][temp.second]);
+                        if(diff < l || diff > r ){
+                            continue;
+                        }
+                        
+                        ch = 1;
+                        vst[nx][ny] = cnt;
+                        q.push(make_pair(nx,ny));
+                        nume += board[nx][ny];
+                        deno++;
+                    }
+                }
+
+                int tmp = nume / deno;
+
+                for(int a = 0 ; a < n ; ++a){
+                    for(int b = 0 ; b < n; ++b){
+                        if(vst[a][b] == cnt) {
+                            board[a][b] = tmp;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return ch;
 }
